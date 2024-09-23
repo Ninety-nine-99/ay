ef1eba3f4b42e4b5532960aa36a78790ee538898
Ninety-nine-99 <127747939+Ninety-nine-99@users.noreply.github.com>
2024-09-23 17:31:19 +0100

    Update firstJava.java
    added constructor and create a object of the class

--- firstJava.java
+++ firstJava.java
@@ -1,5 +1,13 @@
 public class firstJava{
+  String name;
+  firstJava(String name){
+    this.name = name
+  }
   public static void main(String[] args){
+    firstJava obj = new firstJava();
+    
+     System.out.println(obj.firstJava("ayo");)
+    
     System.out.println("new java file");
   }
 }
