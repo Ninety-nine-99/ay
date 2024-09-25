504c6f31d6ce50273a01f94cd12491332a7b0995
Ayotunde <elijaawolowo@gmail.com>
2024-09-25 12:18:28 +0100

    did some changes

--- Savings.java
+++ Savings.java
@@ -1,31 +1,6 @@
-package interfaces;
+public class Savings  {
 
-public class Savings implements IBankAccount, IABCBank {
-
-    @Override
-    public void openAccount() {
-        System.out.println("Open Account");
+    public static void main(String[] args) {
+        System.out.println("Still in a beginner level for now");
     }
-
-    @Override
-    public void closeAccount() {
-        System.out.println("Close Account");
-    }
-
-    @Override
-    public void deposit() {
-        System.out.println("Deposit is Saving Account");
-    }
-
-    @Override
-    public void withdraw() {
-            System.out.println("Withdraw is saving Account");
-    }
-
-    @Override
-    public void balance() {
-        System.out.println("Balance is saving Account");
-    }
-
-    
 }
--- firstJava.java
+++ firstJava.java
@@ -1,13 +1,7 @@
 public class firstJava{
-  String name;
-  firstJava(String name){
-    this.name = name
-  }
+
   public static void main(String[] args){
-    firstJava obj = new firstJava();
+    System.out.println("new java file hello");
     
-     System.out.println(obj.firstJava("ayo");)
-    
-    System.out.println("new java file");
   }
 }
